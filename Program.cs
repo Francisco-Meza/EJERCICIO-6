4a12e12a287d46eddbf5d01b2c7abdddfd2043bc
Francisco-Meza <86315912+Francisco-Meza@users.noreply.github.com>
2021-06-22 23:59:16 -0600

    SE AGREGO EL PROYECTO
    SE ACABA DE AGREGAR EL EJERCICIO NUMERO 6 "SERVICIO DE SALAS"

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EJERCICIO_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EJERCICIO 6");
+        }
+    }
+}
